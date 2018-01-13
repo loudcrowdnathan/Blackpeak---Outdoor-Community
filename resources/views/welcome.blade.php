fe039b4e247a0c3de1d3939fe4a447e9b0f75ebe
Nathan Langer <nathan@loudcrowdit.co.uk>
2018-01-13 01:23:26 +0000

    LESS Adjust

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Blackpeak - The Online Outdoor Community</title>
 
         <!-- Generate CSS with LESS -->
-        <link href="/Libraries/LESS/Generate.php" rel="stylesheet">
+        <link href="/Resources/LESS" rel="stylesheet">
 
         <!-- Fontawesome 5 -->
         <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
@@ -15,14 +16,15 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-
-
-
     </head>
     <body>
+      <header class="navigation-dark">
+        <div class="header-logo">
+          <img src="/img/logo-long-white.png" />
+        </div>
+        <div class="nav-right">
 
+        </div>
+      </header>
     </body>
 </html>
