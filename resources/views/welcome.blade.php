cddeaac15406d59e39b92fa114a16e5bff6421cc
Nathan Langer <nathan@loudcrowdit.co.uk>
2018-01-13 02:08:16 +0000

    Updates LESS & Added SVG JS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Blackpeak - The Online Outdoor Community</title>
 
-        <!-- Generate CSS with LESS -->
+        <!-- LESS -->
         <link href="/Resources/LESS" rel="stylesheet">
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
         <!-- Fontawesome 5 -->
         <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
@@ -16,15 +19,26 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
 
+        <!-- SVG Animations -->
+        <script src="/js/jquery.drawsvg.js"></script>
+
     </head>
     <body>
       <header class="navigation-dark">
         <div class="header-logo">
-          <img src="/img/logo-long-white.png" />
+          <img src="/img/logo-symbol.svg" />
         </div>
         <div class="nav-right">
-
+          <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
         </div>
       </header>
+      <section class="header-splash">
+        
+      </section>
     </body>
 </html>
