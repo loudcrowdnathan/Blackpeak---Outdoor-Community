b528ce1db98f07977b48f33bc29242c614a659c4
Nathan Langer <nathan@loudcrowdit.co.uk>
2018-01-13 00:20:05 +0000

    Initial Commit
    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Generate CSS with LESS -->
+        <link href="/Libraries/LESS/Generate.php" rel="stylesheet">
+
+        <!-- Fontawesome 5 -->
+        <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+
+        <!-- Bootstrap 4 -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+
+
+
+    </head>
+    <body>
+
+    </body>
+</html>
